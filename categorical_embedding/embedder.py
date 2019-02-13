5a7f19e4b5904baf753dd0c27102d5c8aa767aab
Matias Aravena <matias@spikelab.xyz>
2019-02-13 09:26:08 -0300

    add fit_transform

--- categorical_embedding/embedder.py
+++ categorical_embedding/embedder.py
@@ -1,10 +1,9 @@
 from base_model import BaseModel
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler,OneHotEncoder
 import numpy as np
-
-debug = True
-if(debug):
-    import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+from tqdm import tqdm
 
 class Embedder(BaseModel):
     def __init__(self, target_type=None):
@@ -19,27 +18,24 @@ class Embedder(BaseModel):
             raise RuntimeError("<Series> must be of type 'object'")
     
     def _prepare_target(self, y, target_type):
-        try:
-            if target_type == "regression":
-                if  (y.dtypes != "int64" and y.dtypes != "float64"):
-                    raise RuntimeError("For target_type='regression' target must be <int64> or <float64>")
-                else:
-                    scaler = MinMaxScaler()
-                    return scaler.fit_transform(y.values.reshape(-1, 1))
-            elif target_type == "binary_classification":
-                assert y.nunique() == 2, "For target_type='binary_classification' target must have only 2 classes"
-                if (y.dtypes == "bool" or y.dtypes == "object"):
-                    encoder = LabelEncoder()
-                    y = encoder.fit_transform(y)
-                    return y
-                else:
-                    raise RuntimeError("For target_type='binary_classification' target must be <bool> or <object>")
+        if target_type == "regression":
+            if  (y.dtypes != "int64" and y.dtypes != "float64"):
+                raise RuntimeError("For target_type='regression' target must be <int64> or <float64>")
             else:
-                encoder = OneHotEncoder()
-                y = encoder.fit_transform(y.values.reshape(1, -1))
+                scaler = MinMaxScaler()
+                return scaler.fit_transform(y.values.reshape(-1, 1))
+        elif target_type == "binary_classification":
+            assert y.nunique() == 2, "For target_type='binary_classification' target must have only 2 classes" 
+            if (y.dtypes == "bool" or y.dtypes == "object"):
+                encoder = LabelEncoder()
+                y = encoder.fit_transform(y)
                 return y
-        except Exception as e:
-            print("Error:", e)
+            else:
+                raise RuntimeError("For target_type='binary_classification' target must be <bool> or <object>")
+        else:
+            encoder = OneHotEncoder()
+            y = encoder.fit_transform(y.values.reshape(1, -1))
+            return y
 
     def _get_model_params(self, X):
             return np.unique(X).shape[0], min(np.ceil((np.unique(X).shape[0])/2),50)
@@ -51,9 +47,9 @@ class Embedder(BaseModel):
         embedding_layer.columns = col_names
         return embedding_layer
 
-    def _parse_components_index(self, components, categorical_names):
+    def _parse_components_index(self, components, categorical_names, feature_name):
         indexs = pd.Series(categorical_names).to_frame()
-        indexs.columns = ["feature"]
+        indexs.columns = [feature_name]
         return pd.merge(components,indexs, how="left", left_index=True, right_index=True)
 
     def fit(self,X, y):
@@ -63,7 +59,7 @@ class Embedder(BaseModel):
         return pd.DataFrame() with encodings values
         """
         data = X.copy(deep=True)
-        var_name = data.name.replace(" ","_")
+        feature_name = data.name.replace(" ","_")
         target = y.copy(deep=True)
         data, encoder = self._prepare_feature(data)
         y = self._prepare_target(y=y,target_type=self.target_type)
@@ -71,13 +67,35 @@ class Embedder(BaseModel):
         categorical_names = list(encoder.inverse_transform([x for x in range(0,n_classes)]))
         self.model = self.build_model(num_classes=n_classes, vector_size=int(embedding_size))
         self.fit_model(X=data, y=y)
-        components = self._get_components(embedding_size, var_name)
-        components = self._parse_components_index(components, categorical_names)
+        components = self._get_components(embedding_size, feature_name)
+        components = self._parse_components_index(components, categorical_names, feature_name)
         return components
+    
+    def fit_transform(self, X, y, exclude_columns=[]):
+        """
+        X: pandas.DataFrame(): Series with the data
+        y: <string> target variable
+        return pd.DataFrame() with encodings values
+        """
+        data = X.copy(deep=True)
+        selected_cols = data.select_dtypes(['object'])
+        y = data[y]
+        items = [x for x in selected_cols.columns if x not in exclude_columns]
+        selected_cols = selected_cols.filter(items=items)
+        pbar = tqdm(total=len(items))
+        for column in selected_cols:
+            component = self.fit(selected_cols[column], y=y)
+            data = pd.merge(data, component, how="left")
+            pbar.update(1)
+        pbar.close()
+        return data
 
 if __name__ == "__main__":
+    import pandas as pd
     data = pd.read_csv("/Users/maravenag/Desktop/categorical_embedding/Pokemon.csv")
     data.fillna("NaN",inplace=True)
-    e = Embedder(target_type="binary_classification")
-    components = e.fit(data['Type 1'], data['Legendary'])
-    print(components)
+    data = data[['Type_1', 'Type_2', 'Total', 'Legendary']]
+    e = Embedder(target_type="regression")
+    #components = e.fit(data['Type_1'], data['Total'])
+    c = e.fit_transform(data, y="Total", exclude_columns=['Type_2'])
+    print(c.head().T)
