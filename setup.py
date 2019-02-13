2a0a1fa55ea79c10dcb8f3370872e9d880b2e7c4
Matias Aravena <matias@spikelab.xyz>
2019-02-13 11:18:36 -0300

    changes on setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ["scikit-learn==0.20.1",
-                      "pandas==0.23.4",
-                      "numpy==1.15.4",
+REQUIRED_PACKAGES = ["scikit-learn>=0.20.1",
+                      "pandas>=0.23.4",
+                      "numpy>=1.15.4",
                       "keras==2.1.6",
                       "tensorflow==1.8.0",
                       "tqdm"]
